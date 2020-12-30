17ecdbdc6e3e6cc688af76c592ab1b797341d6a0
satnetuser001 <76094506+satnetuser001@users.noreply.github.com>
2020-12-30 09:27:56 +0200

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,23 @@
+<?php
+/*
+https://php720.com/task/7
+Разработайте программу, которая определяла количество прошедших часов по введенным пользователем градусах. Введенное число может быть от 0 до 360.
+*/
+$TimeHour = 0;
+$TimeMinutes = 0;//переменная в которую запишем время в минутах
+$APm = readline("Введите am или pm: \n");
+$Degrees = readline("Введите градусы: \n");
+
+if ($APm == "pm") {
+	$TimeMinutes += 720;//12 часов = 720 минут
+}
+
+$DegreesToMinutes = $Degrees * 2;
+$TimeMinutes += $DegreesToMinutes;
+while ($TimeMinutes >= 60) {
+	$TimeHour ++;
+	$TimeMinutes -= 60;
+}
+
+echo "$Degrees градусов $APm равняется $TimeHour часов $TimeMinutes минут\n";
+?>
