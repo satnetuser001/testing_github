870a76ab9e5fcd86c08771f3347a8561c8610428
satnetuser001 <76094506+satnetuser001@users.noreply.github.com>
2020-12-30 16:45:11 +0200

    Update 3.php

--- 3.php
+++ 3.php
@@ -3,22 +3,23 @@
 https://php720.com/task/7
 Разработайте программу, которая определяла количество прошедших часов по введенным пользователем градусах. Введенное число может быть от 0 до 360.
 */
-$TimeHour = 0;
-$TimeMinutes = 0;//переменная в которую запишем время в минутах
-$APm = readline("Введите am или pm: \n");
-$Degrees = readline("Введите градусы: \n");
-
-if ($APm == "pm") {
-	$TimeMinutes += 720;//12 часов = 720 минут
+function AskDegrees(){
+	$d = readline("Введите градусы от 0 до 360: \n");
+	if ($d >= 0 and $d <= 360) {
+		return $d;
+	}
+	else {
+		return false;
+	}
 }
-
-$DegreesToMinutes = $Degrees * 2;
-$TimeMinutes += $DegreesToMinutes;
-while ($TimeMinutes >= 60) {
-	$TimeHour ++;
-	$TimeMinutes -= 60;
+function DegreesToTime(){
+	$t = AskDegrees();
+	while ($t == false) {
+		echo "Введено неверное значение градусов.\n";
+		$t = AskDegrees();
+	}
+	echo "Ввели $t";
 }
-
-echo "$Degrees градусов $APm равняется $TimeHour часов $TimeMinutes минут\n";
-//test github changes
+echo DegreesToTime() . "\n";
+//echo AskDegrees() . "\n";
 ?>
